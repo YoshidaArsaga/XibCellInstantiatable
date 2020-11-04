3f84feca57daa9fc5ec84477011d1ead570a2f86
Hiroki Yoshida <h_yoshida@arsaga.jp>
2020-11-04 16:52:26 +0900

    Initial Commit

---
+++ XibCellInstantiatable/XibCellInstantiatable.h
@@ -0,0 +1,18 @@
+//
+//  XibCellInstantiatable.h
+//  XibCellInstantiatable
+//
+//  Created by 吉田寛規 on 2020/11/04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XibCellInstantiatable.
+FOUNDATION_EXPORT double XibCellInstantiatableVersionNumber;
+
+//! Project version string for XibCellInstantiatable.
+FOUNDATION_EXPORT const unsigned char XibCellInstantiatableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XibCellInstantiatable/PublicHeader.h>
+
+
